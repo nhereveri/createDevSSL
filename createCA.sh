50af06a0209f961cc860227726f19586ae90d99c
Nelson Hereveri <nelson@hereveri.cl>
2021-10-05 08:37:11 -0300

    Update server script max days

--- createCA.sh
+++ createCA.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MAX_DAYS=1000
+
 RUTA=$(pwd|sed 's/\//\\\//g')
 sed "s/#PASSWD#/${RUTA}/" config.template > config.cnf
 mkdir -p certs crl newcerts private csr
@@ -9,7 +11,7 @@ then
   echo 1000 | tee serial > /dev/null
 fi
 openssl genrsa -out private/ca.key.pem 4096
-openssl req -config config.cnf -key private/ca.key.pem -new -x509 -days 600 -sha256 -extensions v3_ca -out certs/ca.cert.pem
+openssl req -config config.cnf -key private/ca.key.pem -new -x509 -days ${MAX_DAYS} -sha256 -extensions v3_ca -out certs/ca.cert.pem
 openssl x509 -noout -text -in certs/ca.cert.pem
 echo -e "\nRUN THIS COMMAND TO ADD CA Certificate to Keychain:\n\nsudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain certs/ca.cert.pem\n"
 rm -f config.cnf
